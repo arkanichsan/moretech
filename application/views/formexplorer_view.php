d136ed21b3798ab00a5e23d6a0438322ceb34312
Arkan <arkanichsan37@gmail.com>
2022-09-30 17:43:58 +0700

    form explorer

--- application/views/formexplorer_view.php
+++ application/views/formexplorer_view.php
@@ -57,7 +57,7 @@
       <div class="authincation h-100">
         <div class="container h-100">
           <div class="row justify-content-center h-100 align-items-center">
-            <div class="col-md-6">
+            <div class="col-md-12">
               <div class="authincation-content">
                 <div class="row no-gutters">
                   <div class="col-xl-12">
