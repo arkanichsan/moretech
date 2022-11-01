485d9f8299d298602208e60f346bdce1e2960b1b
boedsatria <stribez@gmail.com>
2022-11-01 13:02:28 +0700

    fixing DB COnfig

--- application/config/database.php
+++ application/config/database.php
@@ -74,15 +74,15 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $uri = base_url();
-$host = "localhost";
-$username = "u4814692_dev";
-$password = "100%Id/En";
-$database = "u4814692_moretech";
+$host = "wellracom.cc5wghrbi86u.ap-southeast-1.rds.amazonaws.com";
+$username = "iotstar";
+$password = "wellracom";
+$database = "manager";
 
 if(strpos($uri, 'localhost') !== false) {
-	$username = "root";
-	$password = "";
-	$database = "u4814692_moretech";
+	$username = "iotstar";
+	$password = "wellracom";
+	$database = "manager";
 }
 
 $db['default'] = array(
